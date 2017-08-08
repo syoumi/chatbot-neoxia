388a53aa4ad1d1e54389aaa142879cd9f1a27fb0
mimokyop <mita.oumaima@gmail.com>
2017-08-08 19:24:09 +0100

    Test ai

--- received/ai/handleAiAction.js
+++ received/ai/handleAiAction.js
@@ -18,22 +18,49 @@ var handleAiAction= (senderID, answer) => {
 	switch (action) {
 
 
-    //Scénario 1 : consulter catalogue d'un logement pour achat ou location avec ou sans critères
+    /*
+     * Scénario 1 : consulter catalogue d'un logement pour achat ou location avec ou sans critères
+     */
+
+    //Catalogue ---> Demander quel type logement
 		case "catalogue-action":
 			//sendCatalogueAppartVente(senderID, text);
       var replies = ["Appartement", "Maison", "Villa", "Studio"];
       sendQuickReplies(senderID, text, replies);
 		  break;
+    //Type logement ---> Demander quelle opération
     case "type-logement-action":
-  		//sendCatalogueAppartVente(senderID, text);
       var replies = ["Acheter", "Louer"];
       sendQuickReplies(senderID, text, replies);
   		break;
+    //Opération ---> Demander fixer fourchette
+    case "operation-action":
+      //sendCatalogueAppartVente(senderID, text);
+      var replies = ["Oui", "Non"];
+      sendQuickReplies(senderID, text, replies);
+      break;
+    //Accepte fourchette ---> Demander prix min
+    case "accept-fourchette-action":
+      sendTextMessage(senderID, text);
+      break;
+    //Prix min ---> Demander prix max
+    case "min-fourchette-action":
+      sendTextMessage(senderID, text);
+      break;
+    //Prix max/Refuser fourchette ---> Demander spéficier nbr chambres
+    case "max-fourchette-action", "refuse-fourchette-action":
+      var replies = ["Oui", "Non"];
+      sendQuickReplies(senderID, text, replies);
+      break;
+
 
 
 		default:
 			//unhandled action, just send back the text
 			sendTextMessage(senderID, text);
+
+
+      //sendCatalogueAppartVente(senderID, text);
 	}
 }
 
