00afe5bd80297a905d81fb115f7bec11f30fd7a3
mimokyop <mita.oumaima@gmail.com>
2017-08-16 10:10:09 +0100

    Once again

--- data/salesforce/sendCatalogueAppartement.js
+++ data/salesforce/sendCatalogueAppartement.js
@@ -7,7 +7,7 @@ const {sendButtonMessage} = require('./../../send/fbApi/sendButtonMessage');
 const {sendFileMessage} = require('./../../send/fbApi/sendFileMessage');
 const {sendGenericMessage} = require('./../../send/fbApi/sendGenericMessage');
 const {sendImageMessage} = require('./../../send/fbApi/sendImageMessage');
-const {sendQuickReply} = require('./../../send/fbApi/sendQuickReply');
+const {sendQuickReply} = require('./../../send/fbApi/sendQuickReplies');
 const {sendTextMessage} = require('./../../send/fbApi/sendTextMessage');
 
 
