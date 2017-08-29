4a1182a8ff99817f29092c58ccf2a9a4adde8e0f
mimokyop <mita.oumaima@gmail.com>
2017-08-29 15:52:15 +0100

    Data

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -19,7 +19,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
     sendTextMessage(senderID, text);
     doLogin((conn) => {
     //Search for building
-    var query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__c FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
+    var query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
 
     if(minPrice && maxPrice) {
       query += " AND amount__c >=" + minPrice + " AND amount__c <= " + maxPrice;
@@ -58,7 +58,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
       }
       //Search building in specific neighborhood, if client fixed it
       if(neighborhood){
-        query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__c FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND neighborhood__c = '" + neighborhood + "'";
+        query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND neighborhood__c = '" + neighborhood + "'";
         elements = getProductRecords(conn, query);
         if(elements){
           sendGenericMessage(senderID, elements);
@@ -69,7 +69,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
       }
       //Search all buildings with specific operation
       if(!city && !neighborhood){
-        query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__c FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
+        query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
         elements = getProductRecords(query);
         if(elements){
           sendGenericMessage(senderID, elements);
