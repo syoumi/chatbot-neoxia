1055352a8883d1be04084cf0fbae152e0562aeca
mimokyop <mita.oumaima@gmail.com>
2017-08-29 10:03:50 +0100

    Data

--- include/config.js
+++ include/config.js
@@ -9,9 +9,9 @@ const FB_PAGE_TOKEN = 'EAAEkoZBTgjzIBAH2nAilInhXo8vH34ab0F7HGI61QSusVnCMoh6Q9EJ3
 // Facebook authentification verify token
 const VERIFY_TOKEN = 'SIMPLE_VERIFY_TOKEN';
 // Salesforce login (Ex. test@example.com
-const SF_LOGIN = 'mitaoumaiima@neoxia.ma';
+const SF_LOGIN = 'sfchatbot@neoxia.ma';
 // Salesforce password + security token
-const SF_PASSWORD = 'neoxia1232R5AQHzeTBQPeTciZ7EEiIySh';
+const SF_PASSWORD = '25072017fkmoTsoLT51c4A85Nse88Cc6L4ITA';
 // API.ai client access token
 const API_AI_CLIENT_ACCESS_TOKEN = '47370c9dd09c4ef9bf8dfa664aa86e39';
 // Facebook app secret code
