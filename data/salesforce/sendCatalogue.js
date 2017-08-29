3b951f034d022283b7cba25e0ddfdc957c883f3b
mimokyop <mita.oumaima@gmail.com>
2017-08-29 16:05:50 +0100

    Data

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -19,7 +19,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
     sendTextMessage(senderID, text);
     doLogin((conn) => {
     //Search for building
-    var query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
+    var query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Name AS SalesmanName, Salesman__r.MobilePhone AS SalesmanPhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
 
     if(minPrice && maxPrice) {
       query += " AND amount__c >=" + minPrice + " AND amount__c <= " + maxPrice;
@@ -95,7 +95,7 @@ var getProductRecords = (conn, query) => {
           var photo= record.Image__c;
           var description= "DESCRIPTION_PAYLOAD|" + record.Description__c;
           //var link= record.Link__c;
-          var contact = "CONTACT_PAYLOAD|" + record.Salesman__r.Name + "|" + record.Salesman__r.MobilePhone;
+          var contact = "CONTACT_PAYLOAD|" + record.SalesmanName + "|" + record.SalesmanPhone;
 
           console.log('Salesman', salesman);
           console.log('DESCRIPTION: ', description);
