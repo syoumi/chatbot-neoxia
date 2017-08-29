2670a02b89e9c2338bb55223f3254a9d5ea2eb6b
mimokyop <mita.oumaima@gmail.com>
2017-08-29 10:53:55 +0100

    DATAA

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -42,7 +42,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
     }
     else{
       text = `Nous sommes désolés. Des ${building}s avec les critères mentionnés ci-dessus ne sont pas disponible pour l'instant.\nSi vous n'êtes pas pressé, vous pouvez nous envoyer vos coordonnées afin de vous contacter une fois votre demande est disponible.\nSinon, nous vous proposons des ${building}s qui pourront vous intéresser.`
-      sendBulkTextMessagesWithDelai(senderID, text);
+      sendTextMessagesWithDelai(senderID, text);
 
       //Try to find something may be interested to sind to the client
       //Search building in specific city, if client fixed it
