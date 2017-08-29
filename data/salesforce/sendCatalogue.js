c625fafab8d066e5c2dadfd8ccf7aff882f31f4b
mimokyop <mita.oumaima@gmail.com>
2017-08-29 09:56:45 +0100

    Data

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -14,7 +14,6 @@ const {sendTextMessage} = require('./../../send/fbApi/sendTextMessage');
 
 
 var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nbrRooms, city, neighborhood) => {
-  doLogin((conn) => {
 
     var query = "SELECT Id, Name, price__c, photo__c, link__c FROM "+ building +"__c WHERE type__c = '"+ operation +"'";
 
@@ -32,6 +31,54 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
       query += " AND neighborhood = '" + neighborhood + "'";
     }
 
+    var elements = getRecords(query);
+
+    if(elements.length>0){
+      sendTextMessage(senderID, text);
+      sendGenericMessage(senderID, elements);
+    }
+    else{
+      var messages = [];
+      messages[0] = text;
+      messages[1] = `Nous sommes désolés. Des ${building}s avec les critères mentionnés ci-dessus ne sont pas disponible pour l'instant.\nSi vous n'êtes pas pressé, vous pouvez nous envoyer vos coordonnées afin de vous contacter une fois votre demande est disponible.\nSinon, nous vous proposons des ${building}s qui pourront vous intéresser.`
+        //Try to find something may be interested to sind to the client
+
+       //Search building in specific city, if client fixed it
+      if(city){
+        query = "SELECT Id, Name, price__c, photo__c, link__c FROM "+ building +"__c WHERE type__c = '"+ operation +"' AND city__c = '" + city + "'";
+        elements = getRecords(query);
+        if(elements.length>0){
+          sendGenericMessage(senderID, elements);
+        }
+        else {
+          city = undefined;
+        }
+      }
+      //Search building in specific neighborhood, if client fixed it
+      if(neighborhood){
+        query = "SELECT Id, Name, price__c, photo__c, link__c FROM "+ building +"__c WHERE type__c = '"+ operation +"' AND neighborhood__c = '" + neighborhood + "'";
+        elements = getRecords(query);
+        if(elements.length>0){
+          sendGenericMessage(senderID, elements);
+        }
+        else{
+          neighborhood = undefined;
+        }
+      }
+      //Search all buildings with specific operation
+      if(!city && !neighborhood){
+        query = "SELECT Id, Name, price__c, photo__c, link__c FROM "+ building +"__c WHERE type__c = '"+ operation +"'";
+        elements = getRecords(query);
+        if(elements.length>0){
+          sendGenericMessage(senderID, elements);
+        }
+      }
+    }
+};
+
+
+var getRecords = (query) => {
+  doLogin((conn) => {
     conn.query(query, (err, res) => {
       if (err) { return console.error(err); }
 
@@ -52,7 +99,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
               buttons: [{
                 type: "web_url",
                 url: link,
-                title: "Voir détails"
+                title: "Détails"
                 }, {
                 type: "postback",
                 title: "Contacter",
@@ -63,29 +110,9 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
             elements.push(element);
         }
 
-        if(elements.length>0){
-          sendTextMessage(senderID, text);
-          sendGenericMessage(senderID, elements);
-        }
-        else{
-          var messages = [];
-          messages[0] = text;
-          messages[1] = `Nous sommes désolés. Des ${building}s avec les critères mentionnés ci-dessus ne sont pas disponible pour l'instant.\nSi vous n'êtes pas pressé, vous pouvez nous envoyer vos coordonnées afin de vous contacter une fois votre demande est disponible.\nSinon, nous vous proposons des ${building}s qui pourront vous intéresser.`
-          sendBulkTextMessagesWithDelai(senderID, messages);
-          if(neighborhood){
-            sendCatalogue(senderID, text, building, operation, undefined, undefined, undefined, undefined, neighborhood);
-          }
-          else if(city){
-            sendCatalogue(senderID, text, building, operation, undefined, undefined, undefined, city, undefined);
-          }
-          else{
-            sendCatalogue(senderID, text, building, operation, undefined, undefined, undefined, undefined, undefined);
-          }
-        }
-
-      });
-    });
-};
+        return elements;
+  });
+}
 
 module.exports = {
   sendCatalogue
