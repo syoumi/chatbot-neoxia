3f9cfba65f4af8f0a50c886c72048e4c26e952e5
mimokyop <mita.oumaima@gmail.com>
2017-09-04 10:00:17 +0100

    Connection to Salesforce

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -40,7 +40,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
     var elements = getProductRecords(conn, query);
 
     console.log('ELEMENTS : ', elements);
-    if(elements){
+    if(elements.length!=0){
       sendGenericMessage(senderID, elements);
     }
     else{
@@ -69,7 +69,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
         console.log("Search by neighborhood");
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND neighborhood__c = '" + neighborhood + "'";
         elements = getProductRecords(conn, query);
-        if(elements){
+        if(elements.length!=0){
           sendGenericMessage(senderID, elements);
         }
         else{
@@ -80,7 +80,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
       if(!city && !neighborhood){
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
         elements = getProductRecords(query);
-        if(elements){
+        if(elements.length!=0){
           sendGenericMessage(senderID, elements);
         }
       }
