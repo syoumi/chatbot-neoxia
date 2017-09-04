2767ad2b829760bcb2f314eab6e14115e43ce8fc
mimokyop <mita.oumaima@gmail.com>
2017-09-04 10:25:38 +0100

    Connection to Salesforce

--- send/fbApi/sendTextMessage.js
+++ send/fbApi/sendTextMessage.js
@@ -41,6 +41,7 @@ var sendTextMessageWithDelai = (recipientID, textMessage) => {
   // to simulate bot is typing on
   var delai = parseInt(Math.random() * ( 500 - 50 ) + 50) * textMessage.length;
 
+  console.log("MESSAGE WILL BE SEND IN: ", delai);
   sendTypingOn(recipientID);
 
   setTimeout(() => {
