daa13d161d61702f8e5f14308e9f0f013a5ddeaa
mimokyop <mita.oumaima@gmail.com>
2017-09-04 12:15:45 +0100

    Connection to SF

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -76,24 +76,17 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
           city = undefined;
         }
       }
-      console.log("City: ", city);
-      console.log("Neighborhood: ", neighborhood);
-      console.log('Building: ', building);
-      console.log("Operation: ", operation);
 
       //Search all buildings with specific operation
       if((!city) && (!neighborhood)){
-        console.log('SEND ALL BUILDINGS');
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone  FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
         elements = getProductRecords(conn, query);
         console.log("1- Elements: ", elements);
         setTimeout(() => {
-          console.log("2- Elements: ", elements);
           if(elements.length!=0){
-            console.log("Try to sind elements");
             sendGenericMessage(senderID, elements);
           }
-        }, 5000);
+        }, 10000);
 
       }
     }
