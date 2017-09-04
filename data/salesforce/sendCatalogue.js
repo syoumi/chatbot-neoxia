8f0bb2c60cf7c753553b99e854720f2bf8c155b8
mimokyop <mita.oumaima@gmail.com>
2017-09-04 12:04:38 +0100

    Test GUI

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -84,7 +84,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
         console.log('SEND ALL BUILDINGS');
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
         elements = getProductRecords(conn, query);
-        console.los("Elements: ", elements);
+        console.log("Elements: ", elements);
         if(elements.length!=0){
           console.log("Try to sind elements");
           sendGenericMessage(senderID, elements);
