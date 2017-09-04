6c17b339ad0a7ee3ca9f61a4dc97a10ff64f0111
mimokyop <mita.oumaima@gmail.com>
2017-09-04 16:22:35 +0100

    Connection to SF

--- data/salesforce/handleRequests.js
+++ data/salesforce/handleRequests.js
@@ -3,9 +3,9 @@ const {doLogin} = require('./login');
 
 var addRequest = (senderID, building, operation, minPrice, maxPrice, nbrRooms, city, neighborhood, isTreated) => {
     doLogin((conn) => {
-//       INSERT INTO table_name (column1, column2, column3, ...)
-// VALUES (value1, value2, value3, ...);
       var query = "INSERT INTO Request__c (name, operation__c, isTreated__c, neighborhood__c, city__c, number_of_rooms__c, type__c, maximum_price__c, minimum_price__c, FacebookID__c) VALUES ('" + operation + building + senderID + "', '" + operation + "', " +  isTreated +", '" + neighborhood + "', '" + city +"', " +  nbrRooms +", " +  maxPrice + ", " + minPrice + ", " + senderID + ")";
+      console.log("QUERY: ", query);
+      
       conn.query(query, (err) => {
         if (err) { return console.error(err); }
       });
