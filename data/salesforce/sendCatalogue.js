9392ff18f8be7e6e599f7491758f20d2b52f26f1
mimokyop <mita.oumaima@gmail.com>
2017-09-04 10:17:56 +0100

    Connection to Salesforce

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -80,7 +80,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
 
       //Search all buildings with specific operation
       if((!city) && (!neighborhood)){
-        console.log('City: ,' city);
+        console.log('City: ', city);
         console.log('Neighborhood: ', neighborhood);
         console.log("Search all");
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
