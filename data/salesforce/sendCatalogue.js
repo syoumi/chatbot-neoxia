9d49a136208da12f34fe4c27a4b8405f17c00c3f
mimokyop <mita.oumaima@gmail.com>
2017-09-04 10:27:46 +0100

    Connection to Salesforce

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -55,7 +55,6 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
 
       //Search building in specific neighborhood, if client fixed it
       if(neighborhood){
-        console.log("Search by neighborhood");
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND neighborhood__c = '" + neighborhood + "'";
         elements = getProductRecords(conn, query);
         if(elements.length!=0){
@@ -65,24 +64,21 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
           neighborhood = undefined;
         }
       }
+      
       //Search building in specific city, if client fixed it
       if(city && (!neighborhood)){
-        console.log("Search by city");
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND city__c = '" + city + "'";
         elements = getProductRecords(conn, query);
-        if(elements){
+        if(elements.length!=0){
           sendGenericMessage(senderID, elements);
         }
         else {
           city = undefined;
         }
       }
-      console.log('City: ', city);
-      console.log('Neighborhood: ', neighborhood);
 
       //Search all buildings with specific operation
       if((!city) && (!neighborhood)){
-        console.log("Search all");
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
         elements = getProductRecords(query);
         if(elements.length!=0){
