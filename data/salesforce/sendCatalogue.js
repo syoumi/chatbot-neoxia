aead5564cce77feb225503895807751d2e459344
mimokyop <mita.oumaima@gmail.com>
2017-09-04 10:09:57 +0100

    Connection to Salesforce

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -66,7 +66,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
         }
       }
       //Search building in specific city, if client fixed it
-      if(city && !neighborhood){
+      if(city && (!neighborhood)){
         console.log("Search by city");
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND city__c = '" + city + "'";
         elements = getProductRecords(conn, query);
@@ -79,7 +79,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
       }
 
       //Search all buildings with specific operation
-      if(!city && !neighborhood){
+      if((!city) && (!neighborhood)){
         console.log("Search all");
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
         elements = getProductRecords(query);
