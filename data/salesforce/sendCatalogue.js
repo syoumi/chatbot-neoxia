cca165831d5f5110a12a124263047a84f728a232
Mahdi <Mahdi.syoumi@gmail.com>
2017-09-05 16:09:28 +0100

    ..

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -69,7 +69,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
         console.log('neighborhood is ' , neighborhood);
         if(city && (!neighborhood)){
           query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND city__c = '" + city + "'";
-          getProductRecords(conn, query, callback(elements) => {
+          getProductRecords(conn, query, (elements) => {
             if(elements.length!=0){
               sendGenericMessage(senderID, elements);
             }
@@ -88,7 +88,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
               sendGenericMessage(senderID, elements);
             }
           });
-          
+
         }
       }
     });
