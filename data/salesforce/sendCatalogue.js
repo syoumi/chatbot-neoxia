8989c96dfd196701d6c83656650036b47a7bd7d9
Mahdi <Mahdi.syoumi@gmail.com>
2017-09-05 15:29:26 +0100

    ..

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -56,9 +56,9 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
       //Search building in specific neighborhood, if client fixed it
       console.log('neighborhood!! : ', neighborhood);
       if(neighborhood){
-        console.log('neighborhood2 ' , neighborhood);
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND neighborhood__c = '" + neighborhood + "'";
         elements = getProductRecords(conn, query);
+        console.log('neighborhood2 ' , neighborhood);
 
         setTimeout(() => {
           if(elements.length!=0){
