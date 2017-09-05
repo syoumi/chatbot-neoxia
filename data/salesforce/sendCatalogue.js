c01e17f111d6cf020a3a46ba7f01d54dd6ecf40a
mimokyop <mita.oumaima@gmail.com>
2017-09-05 12:35:00 +0100

    Data

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -52,8 +52,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
       text = `Nous sommes désolés. Des ${building}s avec les critères mentionnés ci-dessus ne sont pas disponible pour l'instant.\nSi vous n'êtes pas pressé, vous pouvez nous envoyer vos coordonnées afin de vous contacter une fois votre demande est disponible.\nSinon, nous vous proposons des ${building}s qui pourront vous intéresser.`
       sendTextMessageWithDelai(senderID, text);
 
-      console.log("neighborhood: ", neighborhood);
-      console.log("city: ", city);
+
 
       //Search building in specific neighborhood, if client fixed it
       if(neighborhood){
@@ -69,8 +68,6 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
         }, 30000);
       }
 
-      console.log("neighborhood: ", neighborhood);
-      console.log("city: ", city);
 
       //Search building in specific city, if client fixed it
       if(city && (!neighborhood)){
@@ -87,9 +84,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
 
       }
 
-      console.log("neighborhood: ", neighborhood);
-      console.log("city: ", city);
-      
+
       //Search all buildings with specific operation
       if((!city) && (!neighborhood)){
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone  FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
