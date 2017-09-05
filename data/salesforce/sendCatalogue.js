161367b086fc3859a26225a3340b86b9988b34d2
mimokyop <mita.oumaima@gmail.com>
2017-09-05 17:39:37 +0100

    Parameters

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -56,7 +56,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
         if(neighborhood){
           query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND neighborhood__c = '" + neighborhood + "'";
           getProductRecords(conn, query, (elements) => {
-            if ( elements.length != 0 ) { // TODO by syoumi
+            if ( elements.length != 0 ) {
               sendGenericMessageWithDelai(senderID, elements, 30000);
             }
             else {
@@ -64,7 +64,6 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
             }
 
             //Search building in specific city, if client fixed it
-            console.log('neighborhood is ' , neighborhood);
             if(city && (!neighborhood)){
               query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND city__c = '" + city + "'";
               getProductRecords(conn, query, (elements) => {
@@ -82,18 +81,14 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
                     if(elements.length!=0){
                       sendGenericMessageWithDelai(senderID, elements, 30000);
                     }
-                  });
-
-                }
-              });
-            }
-          });
-        }
-
-
-
-      }
-    });
+                  });//END getProductRecords 3
+                }//END if 3 : (!city) && (!neighborhood)
+              });//END getProductRecords  2
+            }//END if 2 : (city) && (!neighborhood)
+          });//END getProductRecords  1
+        }//END if 1 : (neighborhood)
+      }//END Else
+    });// END getProductRecords  0
 
   });
 };
@@ -141,8 +136,6 @@ var getProductRecords = (conn, query, callback) => {
 
         }
 
-        console.log('Elements to return ' , elements);
-        // return elements;
         callback(elements);
 
   });
