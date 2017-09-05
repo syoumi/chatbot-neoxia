e3e574d31400a9c1c995059840f6a72b369e2107
mimokyop <mita.oumaima@gmail.com>
2017-09-05 14:18:27 +0100

    Data

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -58,6 +58,8 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
       if(neighborhood){
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND neighborhood__c = '" + neighborhood + "'";
         elements = getProductRecords(conn, query);
+        console.log('Elements: ', elements);
+        console.log('*Neighborhood*');
         setTimeout(() => {
           if(elements.length!=0){
             sendGenericMessage(senderID, elements);
@@ -73,6 +75,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
       if(city && (!neighborhood)){
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND city__c = '" + city + "'";
         elements = getProductRecords(conn, query);
+        console.log('*City*');
         setTimeout(() => {
           if(elements.length!=0){
             sendGenericMessage(senderID, elements);
@@ -89,6 +92,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
       if((!city) && (!neighborhood)){
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone  FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
         elements = getProductRecords(conn, query);
+        console.log('*All*');
         setTimeout(() => {
           if(elements.length!=0){
             sendGenericMessage(senderID, elements);
