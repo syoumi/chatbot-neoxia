aee2d8497da966611bcb25ae3d3a53493297def7
mimokyop <mita.oumaima@gmail.com>
2017-09-05 11:55:35 +0100

    Data

--- data/salesforce/handleRequests.js
+++ data/salesforce/handleRequests.js
@@ -3,8 +3,7 @@ const {doLogin} = require('./login');
 
 var addRequest = (senderID, building, operation, minPrice, maxPrice, nbrRooms, city, neighborhood, isTreated) => {
     doLogin((conn) => {
-
-      conn.query(query, (err) => {
+      conn.query("Request").create({Name: operation+building+senderID }, function(err, res) {
         if (err) { return console.error(err); }
       });
     });
