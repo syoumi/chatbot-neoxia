f64331cca5b51410466400127539942a49c25804
Mahdi <Mahdi.syoumi@gmail.com>
2017-09-05 16:08:06 +0100

    ..

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -37,74 +37,67 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
       query += " AND neighborhood__c = '" + neighborhood + "'";
     }
 
-    var elements = getProductRecords(conn, query);
-
-    setTimeout(() => {
+    getProductRecords(conn, query, (elements) => {
       if(elements.length!=0){
         sendGenericMessage(senderID, elements);
       }
-    else{
-      //Add request
-      //addRequest(senderID, building, operation, minPrice, maxPrice, nbrRooms, city, neighborhood, false);
+      else{
+        //Add request
+        //addRequest(senderID, building, operation, minPrice, maxPrice, nbrRooms, city, neighborhood, false);
 
-      //Try to find something may be interested to sind to the client
-      text = `Nous sommes désolés. Des ${building}s avec les critères mentionnés ci-dessus ne sont pas disponible pour l'instant.\nSi vous n'êtes pas pressé, vous pouvez nous envoyer vos coordonnées afin de vous contacter une fois votre demande est disponible.\nSinon, nous vous proposons des ${building}s qui pourront vous intéresser.`
-      sendTextMessageWithDelai(senderID, text);
+        //Try to find something may be interested to sind to the client
+        text = `Nous sommes désolés. Des ${building}s avec les critères mentionnés ci-dessus ne sont pas disponible pour l'instant.\nSi vous n'êtes pas pressé, vous pouvez nous envoyer vos coordonnées afin de vous contacter une fois votre demande est disponible.\nSinon, nous vous proposons des ${building}s qui pourront vous intéresser.`
+        sendTextMessageWithDelai(senderID, text);
 
 
 
-      //Search building in specific neighborhood, if client fixed it
-      if(neighborhood){
-        query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND neighborhood__c = '" + neighborhood + "'";
-        elements = getProductRecords(conn, query);
+        //Search building in specific neighborhood, if client fixed it
+        if(neighborhood){
+          query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND neighborhood__c = '" + neighborhood + "'";
+          getProductRecords(conn, query, (elements) => {
+            if ( elements.length != 0 ) { // TODO by syoumi
+              sendGenericMessage(senderID, elements);
+            }
+            else {
+                neighborhood = undefined;
+            }
+          });
+        }
 
-        setTimeout(() => {
-          if ( elements.length != 0 ) { // TODO by syoumi
-            sendGenericMessage(senderID, elements);
-          }
-          else {
-              neighborhood = undefined;
-          }
-        }, 30000);
+
+        //Search building in specific city, if client fixed it
+        console.log('neighborhood is ' , neighborhood);
+        if(city && (!neighborhood)){
+          query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND city__c = '" + city + "'";
+          getProductRecords(conn, query, callback(elements) => {
+            if(elements.length!=0){
+              sendGenericMessage(senderID, elements);
+            }
+            else {
+                city = undefined;
+            }
+          });
+        }
+
+
+        //Search all buildings with specific operation
+        if((!city) && (!neighborhood)){
+          query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone  FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
+          getProductRecords(conn, query, (elements) => {
+            if(elements.length!=0){
+              sendGenericMessage(senderID, elements);
+            }
+          });
+          
+        }
       }
+    });
 
-
-      //Search building in specific city, if client fixed it
-      console.log('neighborhood is ' , neighborhood);
-      if(city && (!neighborhood)){
-        query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND city__c = '" + city + "'";
-        elements = getProductRecords(conn, query);
-        console.log('*City*');
-        setTimeout(() => {
-          if(elements.length!=0){
-            sendGenericMessage(senderID, elements);
-          }
-          else {
-              city = undefined;
-          }
-        }, 30000);
-
-      }
-
-
-      //Search all buildings with specific operation
-      if((!city) && (!neighborhood)){
-        query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone  FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
-        elements = getProductRecords(conn, query);
-        console.log('*All*');
-        setTimeout(() => {
-          if(elements.length!=0){
-            sendGenericMessage(senderID, elements);
-          }
-        }, 30000);
-      }
-    }
-  }, 3000);
   });
 };
 
 
-var getProductRecords = (conn, query) => {
+var getProductRecords = (conn, query, callback) => {
  var elements = [];
 
     conn.query(query, (err, res) => {
@@ -148,7 +141,8 @@ var getProductRecords = (conn, query) => {
 
   });
   console.log('Elements to return ' , elements);
-  return elements;
+  // return elements;
+  callback(elements);
 }
 
 module.exports = {
