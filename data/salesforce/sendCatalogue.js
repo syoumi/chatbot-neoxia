72101e88cbcaa16d419468fccd5d3979bcf85685
mimokyop <mita.oumaima@gmail.com>
2017-09-05 14:19:45 +0100

    Data

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -58,8 +58,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
       if(neighborhood){
         query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"' AND neighborhood__c = '" + neighborhood + "'";
         elements = getProductRecords(conn, query);
-        console.log('Elements: ', elements);
-        console.log('*Neighborhood*');
+      
         setTimeout(() => {
           if(elements.length!=0){
             sendGenericMessage(senderID, elements);
