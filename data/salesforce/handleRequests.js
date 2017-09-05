1f038fa013437c10ae0f76fb2b090708dfa65cba
mimokyop <mita.oumaima@gmail.com>
2017-09-05 11:48:15 +0100

    Data

--- data/salesforce/handleRequests.js
+++ data/salesforce/handleRequests.js
@@ -3,8 +3,8 @@ const {doLogin} = require('./login');
 
 var addRequest = (senderID, building, operation, minPrice, maxPrice, nbrRooms, city, neighborhood, isTreated) => {
     doLogin((conn) => {
-      var query = "INSERT INTO lead (type__c, operation__c, name) VALUES ('"+ building + "', '" +  operation+ "', '" + operation + building + "')";
-      conn.query(query, (err, res) => {
+
+      conn.query(query, (err) => {
         if (err) { return console.error(err); }
       });
     });
