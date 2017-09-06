211d2013d2c4908a9538bc43b01ecf57c73c23b3
mimokyop <mita.oumaima@gmail.com>
2017-09-06 11:24:29 +0100

    Add Lead

--- data/salesforce/handleContacts.js
+++ data/salesforce/handleContacts.js
@@ -3,32 +3,36 @@ const {doLogin} = require('./login');
 
 var isContact = (senderID, email) => {
   var res = false;
-  var query = "SELECT FacebookId__c, email FROM Contact";
-  conn.query(query, (err, res) => {
-    if (err) { return console.error(err); }
+  doLogin((conn) => {
+    var query = "SELECT FacebookId__c, email FROM Contact";
+    conn.query(query, (err, res) => {
+      if (err) { return console.error(err); }
 
-      for (var i=0; i<res.records.length; i++) {
-        var record = res.records[i];
-        if( (senderID == record.FacebookId__c) || (email && email == record.email) ){
-          res = true;
+        for (var i=0; i<res.records.length; i++) {
+          var record = res.records[i];
+          if( (senderID == record.FacebookId__c) || (email && email == record.email) ){
+            res = true;
+          }
         }
-      }
+    });
   });
   return res;
 }
 
 var getContact = (senderID, email) => {
   var contact = undefined;
-  var query = "SELECT Name, AccountId, Description, Languages, MobilePhone, DoNotCall, LeadSource, FacebookId__c, email FROM Contact";
-  conn.query(query, (err, res) => {
-    if (err) { return console.error(err); }
+  doLogin((conn) => {
+    var query = "SELECT Name, AccountId, Description, Languages, MobilePhone, DoNotCall, LeadSource, FacebookId__c, email FROM Contact";
+    conn.query(query, (err, res) => {
+      if (err) { return console.error(err); }
 
-      for (var i=0; i<res.records.length; i++) {
-        var record = res.records[i];
-        if( (senderID == record.FacebookId__c) || (email && email == record.email) ){
-          contact = record;
+        for (var i=0; i<res.records.length; i++) {
+          var record = res.records[i];
+          if( (senderID == record.FacebookId__c) || (email && email == record.email) ){
+            contact = record;
+          }
         }
-      }
+    });
   });
   return contact;
 }
