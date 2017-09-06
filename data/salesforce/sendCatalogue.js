4092a0074eaa290d600fb7dbfb30cbf63ad1d9d6
mimokyop <mita.oumaima@gmail.com>
2017-09-06 10:18:38 +0100

    Connection to SF

--- data/salesforce/sendCatalogue.js
+++ data/salesforce/sendCatalogue.js
@@ -16,9 +16,6 @@ const {sendTextMessage} = require('./../../send/fbApi/sendTextMessage');
 
 var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nbrRooms, city, neighborhood, count) => {
 
-    //Send text first
-    sendTextMessageWithDelai(senderID, text);
-
     //Search for building
     var query = "SELECT Id, Name, amount__c, image__c, link__c, Description__c, Salesman__r.Id, Salesman__r.Name, Salesman__r.MobilePhone  FROM product2 WHERE type__c='"+ building +"' AND operation__c = '"+ operation +"'";
 
@@ -39,6 +36,7 @@ var sendCatalogue = (senderID, text, building, operation, minPrice, maxPrice, nb
     getProductRecords(query, (elements) => {
       console.log("ELEMENTS: ", elements);
       if(elements.length!=0){
+        sendTextMessageWithDelai(senderID, text);
         sendGenericMessageWithDelay(senderID, elements, 30000);
       }
       else{
