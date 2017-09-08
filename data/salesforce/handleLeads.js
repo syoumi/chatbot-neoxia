74feb83e3ea87c8cadb19bf9f2c591eae51b8b7e
mimokyop <mita.oumaima@gmail.com>
2017-09-08 10:43:07 +0100

    Task

--- data/salesforce/handleLeads.js
+++ data/salesforce/handleLeads.js
@@ -63,7 +63,7 @@ var updateLead = (senderID, fname, lname, company, city, country, email, phone, 
   getLead(senderID, (leadFound) => {
     if(leadFound){
       doLogin((conn) => {
-        var query = "SELECT Id, firstName, lastName, facebookId__c, company,  city, country, email, Phone, ToConvert FROM lead WHERE FacebookID__c= '" + senderID + "'";
+        var query = "SELECT Id, firstName, lastName, facebookId__c, company,  city, country, email, Phone, ToConvert__c FROM lead WHERE FacebookID__c= '" + senderID + "'";
         conn.query(query)
             .update({ firstName : fname, lastName : lname, company : company, city : city, country: country, email : email, Phone : phone }, 'Lead', function(err, rets) {
               if (err) { return console.error(err); }
