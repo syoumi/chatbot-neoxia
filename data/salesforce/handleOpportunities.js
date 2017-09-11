304128a613eb551e0a5f9915875c6ebd87a497c8
mimokyop <mita.oumaima@gmail.com>
2017-09-11 09:57:23 +0100

    Quote

--- data/salesforce/handleOpportunities.js
+++ data/salesforce/handleOpportunities.js
@@ -5,7 +5,7 @@ const {doLogin} = require('./login');
 var getOpportunity = (accountId, callback) => {
   doLogin((conn) => {
     var opportunity= undefined;
-    var query = "SELECT Id, Name, AccountId FROM Opportunity WHERE AccountId =" + accountId + " LIMIT 1";
+    var query = "SELECT Id, Name, AccountId FROM Opportunity WHERE AccountId = '" + accountId + "' LIMIT 1";
     conn.query(query, (err, res) => {
       if (err) { return console.error(err); }
       opportunity = res.records[0];
