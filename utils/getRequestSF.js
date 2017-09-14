67bdd5d2561bb40fe8574ad8ccb886efdf275c69
mimokyop <mita.oumaima@gmail.com>
2017-09-14 11:47:25 +0100

    Request 2

--- utils/getRequestSF.js
+++ utils/getRequestSF.js
@@ -4,7 +4,7 @@ const {sendProduct} = require('./../data/salesforce/sendCatalogue');
 
 var getRequestSF = (req) => {
     //TODO Get lead/contact's language
-    console.log(req.senderID + '; ' req.productID);
+    console.log(req.senderID + '; ' + req.productID);
     sendProduct(req.senderID, req.productID, 'fr');
 }
 
