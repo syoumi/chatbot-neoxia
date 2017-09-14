17cb082cd3e8e52f7ea6beb389f326425d117423
mimokyop <mita.oumaima@gmail.com>
2017-09-14 17:53:50 +0100

    FB

--- utils/getRequest.js
+++ utils/getRequest.js
@@ -6,13 +6,7 @@ const {getProduct} = require('./../data/salesforce/handleProducts');
 
 const {sharePhoto} = require('./shareOnFB');
 const {shareStatus} = require('./shareOnFB');
-const {shareAlbum} = require('./shareOnFB');
 
-const {FB_PAGE_TOKEN} = require('./../include/config');
-const {FB_PAGE_ID} = require('./../include/config');
-const {FB_ADMIN_TOKEN} = require('./../include/config');
-
-const request = require('request');
 
 
 var getRequestSF = (req) => {
@@ -27,22 +21,9 @@ var getRequestFB = (req) => {
     console.log("POST PRODUCT");
     if(product){
       var text = '**NOUVEAUTE**\n' + product.Name + '\n' + product.Description__c;
-      request({
-        uri: 'https://graph.facebook.com/2027653890797502/feed',
-        qs: {
-          access_token: FB_ADMIN_TOKEN,
-          message: text
-        },
-        method: 'POST'
-      }, (error, response, body) => {
-        if (error) {
-          console.log('Error occured while posting to facebook page.');
-          return console.error('Error occured while posting to facebook page.');
-        }
-
-        console.log('Posted to facebook with status ' , response.statusCode);
-      });
-
+      var image = product.Image__c;
+      shareStatus(text);
+      //sharePhoto(text, image);
     }
 
 
--- utils/shareOnFB.js
+++ utils/shareOnFB.js
@@ -1,18 +1,48 @@
 
+const request = require('request');
 
-var sharePhoto = (product) => {
+const {FB_PAGE_TOKEN} = require('./../include/config');
+const {FB_PAGE_ID} = require('./../include/config');
+const {FB_ADMIN_TOKEN} = require('./../include/config');
 
+var sharePhoto = (text, image) => {
+  request({
+    uri: 'https://graph.facebook.com/2027653890797502/feed',
+    qs: {
+      access_token: FB_ADMIN_TOKEN,
+      message: text
+    },
+    method: 'POST'
+  }, (error, response, body) => {
+    if (error) {
+      console.log('Error occured while posting to facebook page.');
+      return console.error('Error occured while posting to facebook page.');
+    }
+
+    console.log('Posted to facebook with status ' , response.statusCode);
+  });
 }
 
-var shareStatus = (product) => {
+var shareStatus = (text) => {
+  request({
+    uri: 'https://graph.facebook.com/2027653890797502/feed',
+    qs: {
+      access_token: FB_ADMIN_TOKEN,
+      message: text
+    },
+    method: 'POST'
+  }, (error, response, body) => {
+    if (error) {
+      console.log('Error occured while posting to facebook page.');
+      return console.error('Error occured while posting to facebook page.');
+    }
 
+    console.log('Posted to facebook with status ' , response.statusCode);
+  });
 }
 
 
-var shareAlbum = (product) => {
-
-}
 
 module.exports = {
-  sharePhoto, shareStatus, shareAlbum
+  sharePhoto, shareStatus
 }
