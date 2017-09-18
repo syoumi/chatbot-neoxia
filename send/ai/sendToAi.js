336892a65b14927125ca1d6e283f1eae75a92022
Oumaïma MITA <mita.oumaima@gmail.com>
2017-09-18 09:48:08 +0100

    Modifications

--- send/ai/sendToAi.js
+++ send/ai/sendToAi.js
@@ -7,8 +7,9 @@ const {receiveMessage} = require('./../../agentAi/agent');
 
 const {handleAiResponse} = require('./../../received/ai/handleAiResponse');
 
-const {getLead} = require('./../../data/salesforce/handleLeads')
+const {getLead} = require('./../../data/salesforce/handleLeads');
 
+const {getContact} = require('./../../data/salesforce/handleContacts');
 /*
   * @desc      Send message to Agent AI
   * @param     senderID : user's facebookId
@@ -18,25 +19,31 @@ const {getLead} = require('./../../data/salesforce/handleLeads')
 var sendToAi = (senderID, messageText) => {
 	var lang = 'fr';
 
+	//Get lead or contact's language; Default language is french
 	getLead(senderID, (lead) => {
-		if(lead && lead.Language__c) {
-			lang = lead.Language__c;
-		}
+		getContact(senderID, (contact) => {
+			if(contact && contact.Language__c){
+				lang = contact.Language__c;
+			}
+			else if(lead && lead.Language__c) {
+				lang = lead.Language__c;
+			}
 
-		console.log('LANGUAGE: ', lang);
+			console.log('LANGUAGE: ', lang);
 
-		var request = {
-			senderID,
-			text : messageText,
-			lang
-		}
+			var request = {
+				senderID,
+				text : messageText,
+				lang
+			}
 
-		var answer = receiveMessage(request);
+			var answer = receiveMessage(request);
 
-		console.log('Request is ' , request);
-		console.log('Answer is ' , answer);
+			console.log('Request is ' , request);
+			console.log('Answer is ' , answer);
 
-		handleAiResponse(answer.recipientID, answer, lang);
+			handleAiResponse(answer.recipientID, answer, lang);
+		});
 	});
 };
 
